0000c0364ad97d786ed8d180011f66ea19e24826
skdGT <skidru00@gmail.com>
2020-02-21 01:58:33 +0000

    added Wall class

--- tp02/src/main/java/Arena.java
+++ tp02/src/main/java/Arena.java
@@ -6,8 +6,11 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
+import javafx.geometry.Pos;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Arena {
     private int width;
@@ -16,21 +19,43 @@ public class Arena {
     public Arena(int width, int height) {
         this.width = width;
         this.height = height;
+        this.walls = createWalls();
+    }
+
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+
+        return walls;
     }
 
     private Hero hero = new Hero(10, 10);
+    private List<Wall> walls;
 
     /** method to draw on the screen **/
     public void draw(TextGraphics graphics) {
         graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
         graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
 
+        for (Wall wall : walls) {
+            wall.draw(graphics);
+        }
+
         hero.draw(graphics);
     }
 
     private boolean canHeroMove(Position position) {
-        return (position.getX() < this.width && position.getX() >= 0 &&
-                position.getY() < this.height && position.getY() >= 0);
+        return (position.getX() < this.width - 1 && position.getX() >= 1 &&
+                position.getY() < this.height - 1 && position.getY() >= 1);
     }
 
 
@@ -65,5 +90,4 @@ public class Arena {
         }
         return 0;
     }
-
 }
---
+++ tp02/src/main/java/Wall.java
@@ -0,0 +1,22 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public class Wall {
+    private Position position;
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public Wall(int x, int y) {
+        this.position = new Position(x, y);
+    }
+
+    public void draw(TextGraphics graphics) {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#FF0000"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()), "X");
+    }
+}
