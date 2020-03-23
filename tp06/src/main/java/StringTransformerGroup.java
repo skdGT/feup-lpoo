711a85ee8baf21707d78331fc58bcf1d72c66d0f
skdGT <skidru00@gmail.com>
2020-03-23 14:00:41 +0000

    StringTransformerGroup implemented !

---
+++ tp06/src/main/java/StringTransformerGroup.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class StringTransformerGroup implements StringTransformer {
+    List<StringTransformer> transformerList;
+
+    public StringTransformerGroup(List<StringTransformer> transformerList) {
+        this.transformerList = transformerList;
+    }
+
+    @Override
+    public void execute() {
+        for (StringTransformer transformer : transformerList)
+            transformer.execute();
+    }
+
+    @Override
+    public void undo() {
+        for (StringTransformer transformer : transformerList)
+            transformer.undo();
+    }
+}
