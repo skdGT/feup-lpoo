d4a07d1b62ae9573e92403bc8933671fb5efbd34
skdGT <skidru00@gmail.com>
2020-03-31 02:20:57 +0100

    Refactoring - Example2 - Implemented an Interface

---
+++ tp07/src/main/java/com/aor/refactoring/example2/Circle.java
@@ -0,0 +1,25 @@
+package com.aor.refactoring.example2;
+
+public class Circle implements Shape {
+    private double x;
+    private double y;
+    private double radius;
+
+    public Circle(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * this.radius;
+    }
+
+    public void draw(GraphicFramework graphics) {
+        graphics.drawCircle(this.x, this.y, this.radius);
+    }
+}
